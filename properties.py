ca39d138b0c4f5ad0d7d1706500455bf8ff9f676
hippo <binzero@ymail.com>
2015-10-26 15:53:39 +0800

    merge update

--- properties.py
+++ properties.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 GRID_X = 10
 GRID_LEN = 30
 OFFSET = 1
@@ -16,22 +15,3 @@ BKCOLOR = None#'#00b0b0'
 
 # random rock num
 ROCK_NUM = 10
-=======
-GRID_X = 10
-GRID_LEN = 30
-OFFSET = 1
-# the pixel of the entire canvas
-AREA_X = (GRID_LEN + 2 * OFFSET) * GRID_X
-
-# 0,1
-DISPLAY_GRID = 1
-# need colorful snake body? 0 or 1 is valide
-COLORFUL = 1
-# if COLORFUL = 0 ,this color will be used
-BODY_COLOR = 'blue'
-# GUI's background color
-BKCOLOR = None#'#00b0b0'
-
-# random rock num
-ROCK_NUM = 10
->>>>>>> f142e28e7d811fcb550dd728c31323dc931357dd
